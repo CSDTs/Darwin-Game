3812c19d103e421f135ce0ebac6b389db882eedf
janliu <janliu@umich.edu>
2021-02-19 10:57:41 -0500

    player orientation and level and button formatting

--- Player/Player.gd
+++ Player/Player.gd
@@ -38,10 +38,13 @@ func _physics_process(delta):
 	position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity.x != 0:
+		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
+		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
 
 
 #func _on_glow_body_entered(body):
